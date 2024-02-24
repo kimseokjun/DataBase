94d6f51bb89bfd7012573748f23d6b4c31f444b7
kimseokjun <k03s13j@naver.com>
2024-02-24 10:19:26 +0900

    240224 ksj

---
+++ 쿼리 #2.sql
@@ -0,0 +1,52 @@
+# 주석
+
+#CRUD
+
+#Create(insert)
+
+INSERT INTO
+	school_mst(
+		school_code,
+		school_name,
+		create_date,
+		update_date
+		)
+VALUES
+	(0,
+	"부산대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"부경대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"동아대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"경성대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"동명대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"동서대학교",
+	NOW(),
+	NOW()
+	),(0,
+	"동의대학교",
+	NOW(),
+	NOW()
+	),	(0,
+	"부산외국어대학교",
+	NOW(),
+	NOW()
+	)
+	;
+	
+	
+	# Read
+	
